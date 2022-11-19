135b894521c0b4ca1df5682e6757c5946d73e342
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2022-11-19 10:59:01 -0800

    removed js file

--- templates/timer.js
+++
@@ -1,6 +0,0 @@
-function long_timer(hours, mins, secs)
-{
-    const current_time = JSON.parse(current_time);
-    const time = document.getElementById('time')
-    time.innerHTML = current_time;
-}
