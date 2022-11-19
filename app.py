599b18d7ac4e473e83539c212522061d51e42435
Wilson Lee <adovkoder@pop-os.localdomain>
2022-11-19 12:52:56 -0800

    timer functions

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html', current_time = '05 : 30 : 29')
-
-
-def timer(end_hour, end_min, end_sec):
-    # code to calculate hours, minutes, and seconds left
-    print("hello world")
 
 if __name__ == "__main__":
    app.run(debug=True)
