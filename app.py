44ee7b2d2960e9567335c61a053dafbad49fd065
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2022-11-19 14:21:04 -0800

    created home page, can't read from form

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html', current_time = '05 : 30 : 29')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        hours = request.form("hour_input")
+        minutes = request.form("minute_input")
+
+        play_sound = request.form("play_alarm")
+        break_freq = request.form("break_freq")
+
+        print(hours)
+        print(minutes)
+        print(play_sound)
+        print(break_freq)
+
+    return render_template('index.html')
 
 
 def timer(end_hour, end_min, end_sec):
