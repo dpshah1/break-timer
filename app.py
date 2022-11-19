4e2dd0a17b9a786e128e8846668ef4a9c5fe0f64
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2022-11-19 11:07:50 -0800

    updated code

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    jsonify({'current_time' : '05 : 30 : 29'})
     return render_template('index.html', current_time = '05 : 30 : 29')
+
+
+def timer(end_hour, end_min, end_sec):
+    # code to calculate hours, minutes, and seconds left
+    print("hello world")
 
 if __name__ == "__main__":
    app.run(debug=True)
