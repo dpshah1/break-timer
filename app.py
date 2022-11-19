2858160d326075e79da94f42a8992f3c8373b07b
Dhruv Shah <dhruv@Punits-MacBook-Pro.local>
2022-11-19 15:17:19 -0800

    html form in home page works

--- app.py
+++ app.py
@@ -5,16 +5,18 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        hours = request.form("hour_input")
-        minutes = request.form("minute_input")
+        hours = request.form["hour_input"]
+        minutes = request.form["minute_input"]
 
-        play_sound = request.form("play_alarm")
-        break_freq = request.form("break_freq")
+        play_sound = request.form["play_alarm"]
+        break_freq = request.form["break_freq"]
 
         print(hours)
         print(minutes)
         print(play_sound)
         print(break_freq)
+
+        return render_template('session.html')
 
     return render_template('index.html')
 
