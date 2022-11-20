a0b5d162d0e548af8ef46f9a5059b8dd48986c14
Wilson Lee <adovkoder@pop-os.localdomain>
2022-11-19 22:18:24 -0800

    session page2

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def hello_world():
     return render_template('index.html', current_time = '05 : 30 : 29')
+
+@app.route('/session')
+def session():
+    return render_template('session.html')
 
 
 def timer(end_hour, end_min, end_sec):
