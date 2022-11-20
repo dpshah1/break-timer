fe6d57e2c9484333104706c7ed35865ebf42e9a0
dpshah1 <79233346+dpshah1@users.noreply.github.com>
2022-11-20 09:59:12 -0800

    finished website

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 from timer import *
+import random
+from sqlalchemy import create_engine, MetaData, Table, Column, select, insert, and_, update, delete
+import os
 
 
 app = Flask(__name__)
@@ -15,6 +18,10 @@ class Exercise(db.Model):
     name = db.Column(db.String(200), nullable=False)
     description = db.Column(db.String(800), nullable=False)
     video_link = db.Column(db.String(200), nullable=False)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -31,6 +38,8 @@ def home():
         print(break_freq)
 
         current_time = datetime.datetime.now()
+
+        session['work_duration'] = break_freq
 
         session['end_hour'] = ((current_time.hour + hours) % 24)
         session['end_minute'] = ((current_time.minute + minutes) % 60)
@@ -59,12 +68,36 @@ def session_page():
         return "session is over, good job"
 
     if current_time.hour >= session['break_hour'] and current_time.minute >= session['break_minute'] and current_time.second >= session['break_second']:
-        return render_template('session.html')
+        session['poses_done'] = 0
+        return redirect('/break')
 
     time = long_timer(session['end_hour'], session['end_minute'], session['end_second'])
     short_time = short_timer(session['break_hour'], session['break_minute'], session['break_second'])
-    return render_template('session.html', time=time, short_time=short_time)
+    return render_template('session.html', time=time, short_time=short_time, long_percentage=percent(session['duration_hours'], session['duration_minutes'], Time(session['start_hour'], session['start_minute'], session['start_second'])))
+
+@app.route('/break')    
+def break_page():
+    all_entries = [["Childs pose","Kneel on a yoga mat with legs together and slowly sit back onto heels. Extend torso up and bend forward from the hips so your chest rests on your thighs and your forehead rests on the ground in front of you. Let shoulders curl around and rest hands next to your feet with your palms up. Hold this position for 5 6 breaths.","https://www.youtube.com/watch?v=qYvYsFrTI0U&ab_channel=ViveHealth"],
+["Standing forward fold pose","From your standing position bend your knees and hinge forward from your hips reaching your hands for your toes. Remember to exhale as you fold to get a better stretch. Hold this position for 5 breaths then slowly rise back up.","https://www.youtube.com/watch?v=oy2AfcI1CXA&ab_channel=OnnitAcademy"],
+["Eagle pose","Garudasana (Eagle Pose) requires careful focus. You must bend your knees cross your left thigh over your right hook the top of your foot behind your right calf spread the scapula and snug your right elbow into the crook of your left bring your palms to touch lift your elbows and stretch your fingers towards the ceiling.","https://www.youtube.com/watch?v=fC9XQWc6ukk&ab_channel=Howcast"]]
+    current_time = datetime.datetime.now()
+
+    if(int(session['poses_done']) >= 2):
+        session['poses_done'] = 0
+        session['break_hour'] = current_time.hour + 1 if current_time.hour + int(session['work_duration']) > 59 else current_time.hour
+        session['break_minute'] = (current_time.minute + int(session['work_duration'])) % 60
+        session['break_second'] = current_time.second
+        return redirect('/session')
+        
     
+    yoga_id = random.randint(0, 2)
+    print(yoga_id)
+    yoga1 = all_entries[yoga_id]
+
+    session['poses_done'] = int(session['poses_done']) + 1
+
+    print(yoga1)
+    return render_template('break.html', yoga1=yoga1, pose = yoga1[0], description=yoga1[1], video_link=yoga1[2])
 
 if __name__ == "__main__":
    app.run(debug=True)
